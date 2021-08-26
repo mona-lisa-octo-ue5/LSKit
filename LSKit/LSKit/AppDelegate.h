20bf03c4466e476d9fecb6efec85d50ee55cf0ab
shiyulong <shiyulong@jiaoday.com>
2021-08-26 11:02:33 +0800

    project

---
+++ LSKit/LSKit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LSKit
+//
+//  Created by 石玉龙 on 2021/8/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LSKit/LSKit/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LSKit
+//
+//  Created by 石玉龙 on 2021/8/26.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
